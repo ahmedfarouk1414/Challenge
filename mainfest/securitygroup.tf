ae48c2ad2cd78a2b574dd14bc580ab10322d8d6a
ahmedfarouk1414 <ahmed_farouk1414@yahoo.com>
2021-11-07 18:58:23 +0200

    Update securitygroup.tf

--- mainfest/securitygroup.tf
+++ mainfest/securitygroup.tf
@@ -13,14 +13,16 @@ resource "aws_security_group" "myinstance" {
     from_port       = 80
     to_port         = 80
     protocol        = "tcp"
-    security_groups = [aws_security_group.elb-securitygroup.id]
+    #security_groups = [aws_security_group.elb-securitygroup.id]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
  ingress {
     from_port       = 443
     to_port         = 443
     protocol        = "tcp"
-    security_groups = [aws_security_group.elb-securitygroup.id]
+    #security_groups = [aws_security_group.elb-securitygroup.id]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
 
@@ -86,8 +88,21 @@ resource "aws_security_group" "allow-postgres" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-
-
+ ingress {
+    from_port       = 443
+    to_port         = 443
+    protocol        = "tcp"
+    security_groups = [aws_security_group.myinstance-securitygroup.id]
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+ ingress {
+    from_port       = 443
+    to_port         = 443
+    protocol        = "tcp"
+    security_groups = [aws_security_group.myinstance-securitygroup.id]
+    
+  }
 
 
 
